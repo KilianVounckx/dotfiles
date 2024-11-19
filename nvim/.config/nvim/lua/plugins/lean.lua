38ae11c956d2f727ac6908cb919db68930ec9155
KilianVounckx <kilianvounckx@hotmail.be>
2024-11-19 14:08:01 +0100

    :sparkles: Add lean lsp

---
+++ nvim/.config/nvim/lua/plugins/lean.lua
@@ -0,0 +1,10 @@
+return {
+    "Julian/lean.nvim",
+    dependecies = {
+        "neovim/nvim-lspconfig",
+        "nvim-lua/plenary.nvim",
+    },
+    opts = {
+        mappings = true,
+    },
+}
