c7e7ad96717dea85a2748a0b4968e0c03618dedd
KilianVounckx <kilianvounckx@hotmail.be>
2024-11-27 20:37:57 +0100

    :sparkles: Add treesitter support

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = {
+            "gleam",
+            "haskell",
+            "lua",
+            "roc",
+            "rust",
+            "typst",
+            "zig",
+        },
+    },
+}
