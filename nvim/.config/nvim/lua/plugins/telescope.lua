8ae03897ec1b31edf81d486c1f8ede2a09d16a8c
Kilian Vounckx <kilianvounckx@hotmail.be>
2025-03-31 09:26:19 +0200

    :sparkles: Add find_files fallback for git_files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,25 @@
+local is_inside_work_tree = {}
+
+local function project_files()
+    local cwd = vim.fn.getcwd()
+    if is_inside_work_tree[cwd] == nil then
+        vim.fn.system("git rev-parse --is-inside-work-tree")
+        is_inside_work_tree[cwd] = vim.v.shell_error == 0
+    end
+    if is_inside_work_tree[cwd] then
+        require("telescope.builtin").git_files({ show_untracked = true })
+    else
+        require("telescope.builtin").find_files()
+    end
+end
+
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
         {
             "<leader>f",
-            "<cmd>Telescope git_files show_untracked=true<cr>",
+            project_files,
             mode = "n",
             desc = "Telescope find files",
         },
