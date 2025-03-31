d35b4f8c844a8a1877836b36b9e1e64189702c37
Kilian Vounckx <kilianvounckx@hotmail.be>
2025-03-31 08:37:48 +0200

    :sparkles: Update telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,25 +3,28 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
         {
-            "<leader>f", "<cmd>Telescope find_files<cr>", mode = "n",
+            "<leader>f",
+            "<cmd>Telescope git_files show_untracked=true<cr>",
+            mode = "n",
             desc = "Telescope find files",
         },
         {
-            "<leader>s", "<cmd>Telescope live_grep<cr>", mode = "n",
+            "<leader>s",
+            "<cmd>Telescope live_grep<cr>",
+            mode = "n",
             desc = "Telescope live grep search",
         },
         {
-            "<leader>b", "<cmd>Telescope buffers<cr>", mode = "n",
+            "<leader>b",
+            "<cmd>Telescope buffers<cr>",
+            mode = "n",
             desc = "Telescope buffers",
         },
         {
-            "<leader>h", "<cmd>Telescope help_tags<cr>", mode = "n",
+            "<leader>h",
+            "<cmd>Telescope help_tags<cr>",
+            mode = "n",
             desc = "Telescope help tags",
-        },
-    },
-    opts = {
-        defaults = {
-            initial_mode = "normal",
         },
     },
 }
