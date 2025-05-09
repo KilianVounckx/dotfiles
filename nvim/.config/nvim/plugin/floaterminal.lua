75524d9c8b59c24047930d16fe6dc8d3efbd2972
Kilian Vounckx <kilianvounckx@hotmail.be>
2025-05-09 09:44:46 +0200

    :art: Move expression to one line

--- nvim/.config/nvim/plugin/floaterminal.lua
+++ nvim/.config/nvim/plugin/floaterminal.lua
@@ -62,10 +62,6 @@ end
 
 vim.api.nvim_create_user_command("Floaterminal", toggle_terminal, {})
 vim.keymap.set({ "n", "t" }, "<leader>tt", toggle_terminal)
-vim.keymap.set(
-    "t",
-    "<leader>cc",
-    clear_terminal
-)
+vim.keymap.set("t", "<leader>cc", clear_terminal)
 
 vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>")
