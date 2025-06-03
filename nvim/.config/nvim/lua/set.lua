499bf16fd740989630c70bb902b081ed2f91e6d1
Kilian Vounckx <kilianvounckx@hotmail.be>
2025-06-03 17:43:43 +0200

    :sparkles: Add auto file reloading in vim

--- nvim/.config/nvim/lua/set.lua
+++ nvim/.config/nvim/lua/set.lua
@@ -61,3 +61,14 @@ vim.filetype.add({
         kk = "koka",
     },
 })
+
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({
+    "BufEnter",
+    "CursorHold",
+    "CursorHoldI",
+    "FocusGained",
+}, {
+    command = "if mode() != 'c' | checktime | endif",
+    pattern = "*",
+})
